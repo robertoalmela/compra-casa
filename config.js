a93d2c07da8dc53cf56d6f33d20e353f4ef7da39
Roberto Almela López <robertoalmela300@gmail.com>
2026-06-21 23:31:40 +0200

    cambiar miembros por defecto: Roberto, Adrián, María

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.APP_CONFIG = {
   supabaseAnonKey: 'sb_publishable_bK76qjB1ZtGBsgs_zsUTow_G9T-LQrZ',
   householdInviteCode: 'casa-roberto',
   householdName: 'Casa',
-  defaultMembers: ['Roberto', 'Hermano 1', 'Hermano 2']
+  defaultMembers: ['Roberto', 'Adrián', 'María']
 };
